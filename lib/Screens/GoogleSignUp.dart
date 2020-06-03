5f494a16d12104b6684e3d16fb852b764d330171
Bennett567 <dani.bohm1234@gmail.com>
2020-06-03 16:54:13 +0200

    logos

--- lib/Screens/GoogleSignUp.dart
+++ lib/Screens/GoogleSignUp.dart
@@ -7,62 +7,62 @@ import 'package:loginmodule/Screens/Classrooms.dart';
 import 'package:loginmodule/Services/Auth.dart';
 import 'package:loginmodule/Screens/Classrooms.dart';
 
-
 class GoogleSignUp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blueAccent,
+      backgroundColor: Colors.lightBlueAccent,
       body: Center(
         child: Container(
-          child: Column(
-            children: <Widget>[
-              Container(
-                height: (MediaQuery.of(context).size.height) / 5,
-                width: MediaQuery.of(context).size.width,
-              ),
-              Container(
-                height: (MediaQuery.of(context).size.height) / 5,
-                width: MediaQuery.of(context).size.width,
-                child: Center(
-                  child: Text(
-                    "GyakorlApp",
-                    style: TextStyle(color: Colors.white, fontSize: 50),
-                  ),
+          child: Column(children: <Widget>[
+            Container(
+              height: (MediaQuery.of(context).size.height) / 5,
+              width: MediaQuery.of(context).size.width,
+            ),
+            Container(
+              height: (MediaQuery.of(context).size.height) / 5,
+              width: MediaQuery.of(context).size.width,
+              child: Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Image(
+                      image: AssetImage(
+                          'C:/Users/Dani/Desktop/gyakorlappreal/lib/UI/logo.png')),
                 ),
+//                  child: Text(
+//                    "GyakorlApp",
+//                    style: TextStyle(color: Colors.white, fontSize: 50),
               ),
-              RaisedButton(
-                onPressed: ()async  {
-                  await authService.testSignInWithGoogle();
-                  Navigator.push(
-                      context,
+            ),
+            RaisedButton(
+              onPressed: () async {
+                await authService.testSignInWithGoogle();
+                Navigator.push(
+                    context,
                     MaterialPageRoute(
-
-                        builder: (context) => ScrollableClassroom())
-                  );},
-                elevation: 20.0,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(10))),
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.only(
-                          right: 15.0, top: 10.0, bottom: 10.0),
-                      child: Icon(FontAwesomeIcons.google),
-                    ),
-                    Text(
-                      "Bejelentkezés Google fiókkal",
-                      style: TextStyle(fontSize: 15),
-                    )
-                  ],
-                ),
-              )
-            ],
-          ),
+                        builder: (context) => ScrollableClassroom()));
+              },
+              elevation: 20.0,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(10))),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(
+                        right: 15.0, top: 10.0, bottom: 10.0),
+                    child: Icon(FontAwesomeIcons.google),
+                  ),
+                  Text(
+                    "Bejelentkezés Google fiókkal",
+                    style: TextStyle(fontSize: 15),
+                  )
+                ],
+              ),
+            ),
+          ]),
         ),
       ),
     );
   }
 }
-
