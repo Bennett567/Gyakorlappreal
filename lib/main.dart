1b23befe1421c71070a2e3d3008e49e5be347013
Bennett567 <dani.bohm1234@gmail.com>
2020-06-06 18:58:46 +0200

    nemforgos

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:loginmodule/Screens/AnswerCorrect.dart';
 import 'package:loginmodule/Screens/GoogleSignUp.dart';
 import 'package:loginmodule/Screens/InClassroom.dart';
@@ -7,7 +8,10 @@ import 'package:loginmodule/Screens/Classrooms.dart';
 import 'package:loginmodule/Screens/Valaszolo.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+  .then((_){
   runApp(
-    MaterialApp(title: 'Gyakorlapp', home: GoogleSignUp()),
-  );
+    MaterialApp(title: 'Gyakorlapp', home: GoogleSignUp())
+  );});
 }
